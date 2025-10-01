0879e4c6c8a1bbec8d0126159f0aec018c2cebb4
MPavithra03 <pavithra.mm76@gmail.com>
2025-10-01 20:51:24 +0530

    Fix next.config export for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
+  // other config here
 };
-module.exports = nextConfig;
 
-
+export default nextConfig;
